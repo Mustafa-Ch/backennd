e77b3931b31fc73eaf4ae762b68798fe721d7d39
Mustafa Shahid <mustufachohan86@gmail.com>
2025-08-04 03:42:18 +0500

    ...

--- src/app.module.ts
+++ src/app.module.ts
@@ -14,16 +14,16 @@ import { SpeechModule } from './speech/speech.module';
 @Module({
   imports: [
     TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'bookd',
-      database: 'bookd',
-      entities: [__dirname + '/../**/*.entity.{ts,js}'],
+  type: 'postgres',
+  host: process.env.DB_HOST,
+  port: 5432,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+    autoLoadEntities: true,
+  synchronize: true,
+}),
 
-      synchronize: true,
-    }),
     ConfigModule.forRoot({
       isGlobal: true,
     }),
