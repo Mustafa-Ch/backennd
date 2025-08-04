0e348785cf95bcb8a0bbaf521a2d5646137c3ee3
Mustafa Shahid <141426131+Mustafa-Ch@users.noreply.github.com>
2025-08-04 17:22:08 +0500

    Update google-calendar.module.ts

--- src/google-calendar/google-calendar.module.ts
+++ src/google-calendar/google-calendar.module.ts
@@ -1,11 +1,11 @@
 import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { CalendarService } from './google-calendar.service';
 import { GoogleCalendarController } from './google-calendar.controller';
-import { AuthMiddleware } from 'utils/middleware';
+import { AuthMiddleware } from '../../utils/middleware';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { GoogleCalendarToken } from './entities/google-calendar.entity';
 import { User } from 'src/user/entities/user.entity';
-import { AuthModule } from 'src/user/user.module';
+import { AuthModule } from '../user/user.module';
 
 @Module({
   imports: [TypeOrmModule.forFeature([GoogleCalendarToken, User]), AuthModule],
