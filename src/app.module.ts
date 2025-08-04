afadf2091364d222ad51d9e90f810c81cd33df7f
Mustafa Shahid <141426131+Mustafa-Ch@users.noreply.github.com>
2025-08-04 14:56:08 +0500

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -13,15 +13,20 @@ import { SpeechModule } from './speech/speech.module';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
+   TypeOrmModule.forRoot({
   type: 'postgres',
-  host: process.env.DB_HOST,
-  port: 5432,
-  username: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-    autoLoadEntities: true,
-  synchronize: true,
+  host: process.env.POSTGRES_HOST,
+  port: Number(process.env.POSTGRES_PORT),
+  username: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  database: process.env.POSTGRES_DB,
+  autoLoadEntities:true,
+  ssl: true,
+  extra: {
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  },
 }),
 
     ConfigModule.forRoot({
