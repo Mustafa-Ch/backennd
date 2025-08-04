46cb8833c5075535b074384a84be8a44ee184995
Mustafa Shahid <141426131+Mustafa-Ch@users.noreply.github.com>
2025-08-04 18:11:53 +0500

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
-
+import { ExpressAdapter } from '@nestjs/platform-express';
+import * as express from 'express';
+const server = express();
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.use(cookieParser());
+  const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
+   app.use(cookieParser());
   await app.listen(3000);
 }
 bootstrap();
